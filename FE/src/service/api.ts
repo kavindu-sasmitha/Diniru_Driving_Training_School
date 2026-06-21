d937c4cf28c28bfd918c9bb8190f77ec26ba2bc3
kavindu-sasmitha <Kavindusasmitha20@gmail.com>
2026-06-21 15:57:54 +0530

    add url

--- FE/src/service/api.ts
+++ FE/src/service/api.ts
@@ -1,23 +1,18 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "diniru-driving-training-school.vercel.app/api/v1"
+  baseURL: "https://diniru-driving-training-school-qg.vercel.app/api/v1"
 })
 
 const PUBLIC_ENDPOINTS = ["/auth/login", "/auth/register", "/auth/refresh"]
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("ACCESS_TOKEN")
-
   const isPublic = PUBLIC_ENDPOINTS.some((url) => config.url?.includes(url))
-
   if (!isPublic && token) {
     config.headers.Authorization = `Bearer ${token}`
   }
-
   return config
 })
-
-// api.interceptors.response
 
 export default api
